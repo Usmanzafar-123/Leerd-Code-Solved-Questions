30378e32684cee3c1201b42f7e1532ad11fcd4dc
Usmanzafar-123 <115619174+Usmanzafar-123@users.noreply.github.com>
2023-07-01 16:28:49 +0530

    Time: 0 ms (100%), Space: 6.2 MB (36.96%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -14,9 +14,28 @@ class Solution {
         dp[n]= c(n-1,dp)+c(n-2,dp);
         return dp[n];
     }
+
+    int d(int n){
+        int a=1;
+        int b=2;
+         if(n==1){
+            return a;
+        }
+        if(n==2){
+            return b;
+        }
+        int c=0;
+        for(int i=3;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
 public:
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-       return c(n,dp);
+    //    return c(n,dp);
+       return d(n);
     }
 };
