65df749f8123e295f744db54d8f2efc2b4bfebd5
Usmanzafar-123 <115619174+Usmanzafar-123@users.noreply.github.com>
2023-09-22 08:58:10 +0530

    Time: 3 ms (49.1%), Space: 6.8 MB (10.03%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n1=s.length();
+        int n2=t.length();
+int b=0;
+        int i=0,j=0;
+        while(i<n1 && j<n2){
+            if(s[i]==t[j]){
+                b++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+          
+        }
+          if(b==n1){
+                return true;
+            }
+        cout<<b<<n1;
+        return false;
+        
+    }
+};
